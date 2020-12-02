797aa2e9ba387370b56e696e34f42ac940dc9081
Rachel Gonzalez <rachel.gonzalez328@gmail.com>
2020-12-01 23:11:58 -0500

    Update DnD.py

--- DnD.py
+++ DnD.py
@@ -2,8 +2,8 @@ from random import randint
 
 print("Welcome to Rachel Gonzalez's integration project!")
 print("This is a simple text and turn base fight game")
-print("Your only options are to fight or run, there's no distinction between winning from running or defeating your "
-      "opponent yet")
+print("Your only options are to fight, run or use an item, there's no distinction between winning from running or "
+      "defeating your opponent yet")
 
 
 # figured out classes with https://www.w3schools.com/python/python_classes.asp
@@ -34,40 +34,51 @@ def roll(mob, user):
     user.hit_roll = randint(1, 10)
 
 
-def items(mob, user):
+def items(mob, user, count):
     i = 1
     for x in user.items_list:
         print(str(i) + ". " + str(x))
         i += 1
-
     item_number = input("Enter the item number to : ")
     if item_number == "1":
         for x in player.items_list[0]:
             if x.isdigit():
+                if int(x) <= 0:
+                    del player.items_list[0]
                 player.items_list[0] = "PokeBall(" + (str(int(x) - 1)) + "x)"
-                print("You used a poe ball!")
+                print("You used a PokeBall!")
+                pokeball(mob, count)
+
                 break
     elif item_number == "2":
         player.hit_points += 20
-
         if player.hit_points > 50:
             player.hit_points = 50
-
         print("\nPlayer health bar:" + "■" * user.hit_points)
         print("You are now at " + str(player.hit_points) + " HP")
+    else:
+        print("Try a again, that wasn't a valid input")
+
+
+def pokeball(mob, count):
+    if mob.hit_points < (mob.hit_points * .5) and count > 3:
+        print("Caught " + mob.name + "!")
 
 
 def combat(mob, user):
+    count = 0
     while mob.hit_points > 0:
         roll(mob, user)
         encounter_response = input("\nEnter 'fight', 'run' or 'items': ")
 
         if encounter_response == "fight":
+            count += 1
             # The player swings first
             if user.strength_roll >= mob.armor_class:
                 print("\nYou launch your self forward in a fearsome attack")
                 print(mob.name + " took " + str(user.hit_roll) + " points of damage!")  # tells damage the user took
                 mob.hit_points -= user.hit_roll  # removes health from the user depending on the roll of the Mob
+                print("\nOpponent health bar:" + "■" * mob.hit_points)
                 if mob.hit_points <= 0:
                     break
             else:
@@ -94,11 +105,39 @@ def combat(mob, user):
                 print("\nPlayer health bar:" + "■" * user.hit_points)
                 print("Your health went down to " + str(user.hit_points) + "! ")
         elif encounter_response == "items":
-            items(mob, user)
+            items(mob, user, count)
 
 
-player = Player("Bruenor", 18, 50, (randint(1, 20) + 1), (randint(1, 10)), ["PokeBall(3x)", "Healing Potion"])
-boar = Mob("Boar", 11, 13, (randint(1, 10) + 1), randint(1, 10))
+def input_geuss():
+    while True:
+        try:
+            guess = int(input("Guess the Number: "))
+        except ValueError:
+            print("Not an integer! Try again.")
+            continue
+        else:
+            return guess
+
+
+def bonus():
+    print("\nBut first! Guess the random number for a some serotonin and nothing ")
+    random = (randint(1, 100))
+    if random % 2 == 0:
+        print("The number is even")
+    else:
+        print("The number is odd")
+
+    guess = input_geuss()
+
+    if guess != random:
+        print("You didn't guess it!")
+    else:
+        print("You got it")
+
+
+player = Player("Charmander", 18, 39, (randint(1, 20) + 1), (randint(1, 10)), ["PokeBall(3x)", "Healing Potion"])
+boar = Mob("Bulbasaur", 11, 45, (randint(1, 20) + 1), randint(1, 10))
+bonus()
 combat(boar, player)
 
 if player.hit_points <= 0:
@@ -106,16 +145,9 @@ if player.hit_points <= 0:
 else:
     print("You Won!\n")
 
-
 # yet to implement naturally
-addition = 2 + 2  # adds numbers I need to add a healing item
-exponentiation = 2**2  # raises a number to the power of another
+exponentiation = 2 ** 2  # raises a number to the power of another
 floor = 77 // 17  # divides a number wile rounding down to a whole number
-modulus = 77 % 17  # Returns the remainder resulting from the division
 
-print("Addition: " + str(addition))
 print("Exponentiation: " + str(exponentiation))
 print("Floor: " + str(floor))
-print("Modulus: " + str(modulus))
-
-print("Done " * 10)  # Needed clear sign that program ended for testing
